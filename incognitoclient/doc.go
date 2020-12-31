333ab8027c3629cedabd08e5d81bb9dbacaf642e
HoangNguyen <hoang@incognito.org>
2020-12-31 10:53:22 +0700

    update docs

--- incognitoclient/doc.go
+++ incognitoclient/doc.go
@@ -40,6 +40,16 @@ Example:
 		blockInfo := NewBlockInfo(publicIncognito)
 		wallet := NewWallet(publicIncognito, blockInfo)
 
+		//create new a wallet
+        paymentAddress, pubkey, readonlyKey, privateKey, validatorKey, shardId , _ := wallet.CreateWallet()
+        fmt.Println("payment adresss", paymentAddress)
+        fmt.Println("public key", pubkey)
+        fmt.Println("readonly key", readonlyKey)
+        fmt.Println("private key", privateKey)
+        fmt.Println("validator key", validatorKey)
+        fmt.Println("shard id", shardId)
+
+        //send prv
 		listPaymentAddresses := entity.WalletSend{
 			Type: 0,
 			PaymentAddresses: map[string]uint64{
